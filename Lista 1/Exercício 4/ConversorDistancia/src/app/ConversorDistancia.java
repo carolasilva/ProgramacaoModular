ffdd968d3e250d912e82e698bbd35f6d9f1da24b
Carol <carol.alves99@live.com>
2019-08-25 18:17:15 -0300

    Exercício 4
    Finalizado exercício 4

---
+++ Lista 1/Exercício 4/ConversorDistancia/src/app/ConversorDistancia.java
@@ -0,0 +1,55 @@
+package app;
+
+public class ConversorDistancia {
+	public static final double TAXA_DE_PES_PARA_METROS = 0.3048;
+	public static final double TAXA_DE_PES_PARA_POLEGADAS = 12;
+	public static final double TAXA_DE_METROS_PARA_CENTIMETROS = 100;
+	
+	public static double converterPesParaMetros(double valor) {
+		return valor * TAXA_DE_PES_PARA_METROS;
+	}
+
+	public static double converterPesParaPolegadas(double valor) {
+		return valor * TAXA_DE_PES_PARA_POLEGADAS;
+	}
+
+	public static double converterPesParaCentimetros(double valor) {
+		return (valor * TAXA_DE_PES_PARA_METROS) * TAXA_DE_METROS_PARA_CENTIMETROS;
+	}
+
+	public static double converterPolegadasParaMetros(double valor) {
+		return (valor / TAXA_DE_PES_PARA_POLEGADAS) * TAXA_DE_PES_PARA_METROS;
+	}
+
+	public static double converterPolegadasParaPes(double valor) {
+		return valor / TAXA_DE_PES_PARA_POLEGADAS;
+	}
+
+	public static double converterPolegadasParaCentimetros(double valor) {
+		return ((valor / TAXA_DE_PES_PARA_POLEGADAS) * TAXA_DE_PES_PARA_METROS) * TAXA_DE_METROS_PARA_CENTIMETROS;
+	}
+
+	public static double converterMetrosParaPes(double valor) {
+		return valor / TAXA_DE_PES_PARA_METROS;
+	}
+
+	public static double converterMetrosParaPolegadas(double valor) {
+		return (valor / TAXA_DE_PES_PARA_METROS) * TAXA_DE_PES_PARA_POLEGADAS;
+	}
+
+	public static double converterMetrosParaCentimetros(double valor) {
+		return valor * TAXA_DE_METROS_PARA_CENTIMETROS;
+	}
+
+	public static double converterCentimetrosParaMetros(double valor) {
+		return valor / TAXA_DE_METROS_PARA_CENTIMETROS;
+	}
+
+	public static double converterCentimetrosParaPes(double valor) {
+		return (valor / TAXA_DE_METROS_PARA_CENTIMETROS) / TAXA_DE_PES_PARA_METROS;
+	}
+
+	public static double converterCentimetrosParaPolegadas(double valor) {
+		return ((valor / TAXA_DE_METROS_PARA_CENTIMETROS) / TAXA_DE_PES_PARA_METROS) * TAXA_DE_PES_PARA_POLEGADAS;
+	}
+}
